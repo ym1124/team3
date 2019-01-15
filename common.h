18a98d75c712d10380af0e07d443cdc9b658fb99
motsu_SiRO <45837800+motsuSiRO@users.noreply.github.com>
2019-01-15 16:10:45 +0900

    こもん　まいふぁんく

---
+++ common.h
@@ -0,0 +1,61 @@
+#ifndef _COMMON_H_
+#define	_COMMON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <math.h>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+
+#define PI	(3.141592f)
+
+//--------------------------------
+//	定数
+//--------------------------------
+#define SCREEN_WIDTH (1440)
+#define SCREEN_HEIGHT (810)
+
+
+struct Color
+{
+	unsigned int White;
+	unsigned int Black;
+	unsigned int Red;
+	unsigned int Green;
+	unsigned int Blue;
+	unsigned int Yellow;
+
+	Color();
+};
+
+extern Color color;
+extern bool gameexit;
+extern int framerate;
+extern bool debugFlg;
+
+void InputKey();
+void InputJoypad();
+void InputAllKey();
+double ToRadian(double deg);
+
+//--------------------------------
+//	マクロ関数
+//--------------------------------
+
+/* ==========  入力関係（トリガー）  ========== */
+// キー入力
+extern int key[256]; // 0:入力されていない 1:入力された瞬間 2:入力されている
+extern int keyall;
+extern int PAD1;
+extern bool slow_flg;
+
+/* ==========  角度変換用  ========== */
+//#define ToRadian(x) FromDgreeToRadian(x)
+#define ToDegree(x) XMConvertToDegrees(x)
+
+#endif // !_COMMON_H_
